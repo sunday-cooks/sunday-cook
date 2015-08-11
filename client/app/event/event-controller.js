e55e91ef4965c854dda7085255fb28e7020985f8
Phil Keys <phil@phillipkeys.com>
2015-08-10 19:24:29 -0700

    [modify] main event controller and html to work with angular-material layout settings

--- client/app/event/event-controller.js
+++ client/app/event/event-controller.js
@@ -1,4 +1,4 @@
-angular.module("event", ['video', 'chat', 'step', 'steps'])
+angular.module("event", ['video', 'chat', 'step', 'steps', 'stepPercent', 'ngMaterial'])
   .controller("eventCtrl", function ($scope) {
     $scope.message = "hello";
   });
