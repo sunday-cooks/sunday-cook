d173435c939690faf5e3ef68b2c8b04c972b329f
Kamron Batman <kamron.batman@gmail.com>
2015-08-13 21:10:21 -0700

    [Fix] Fixes collections so that they can be used properly. Adds missing extended methods for future API endpoints.

--- server/bookshelf/collections/chatmessages.js
+++ server/bookshelf/collections/chatmessages.js
@@ -1,8 +1,14 @@
-var db = require('../config');
-require('../models/chatmessage');
+var db          = require( '../config' ),
+    ChatMessage = require( '../models/chatmessage' );
 
 var ChatMessages = db.Collection.extend( {
-  model: 'ChatMessage',
+  model: ChatMessage,
+}, {
+
+  fetchMessages: function ( eventId ) {
+    return new this().where( { 'event_id': eventId } ).fetch();
+  },
+
 });
 
 module.exports = db.collection( 'ChatMessages', ChatMessages );
