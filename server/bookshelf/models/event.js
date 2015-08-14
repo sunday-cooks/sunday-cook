39414bb18b68dbc745334bb7ca5b21dd6ccba06c
Kamron Batman <kamron.batman@gmail.com>
2015-08-13 19:04:27 -0700

    [Fix] Fixes /api/events/:eventid to properly return all data as a JSON object.

--- server/bookshelf/models/event.js
+++ server/bookshelf/models/event.js
@@ -1,4 +1,5 @@
-var db = require( '../config' );
+var db        = require( '../config' ),
+    Promise  = require( 'bluebird' );
 require( './step' );
 require( './user' );
 require( './ingredient' );
@@ -31,17 +32,22 @@ var Event = db.Model.extend( {
     return this.hasMany( 'ChatMessage' );
   },
 
+  // This is a promise!!!
   eventDetails: function () {
     var event = {};
 
     event.name = this.get( 'name' );
     event.description = this.get( 'description' );
     event.ingredients = this.related( 'ingredients' ).toJSON();
-    event.steps = this.related( 'steps' ).toJSON();
     event.chef = this.related( 'chef' ).toJSON();
     event.tools = this.related( 'tools' ).toJSON();
 
-    return event;
+    return this.related( 'steps' ).fetch( { withRelated: [ 'ingredients', 'tools' ] } )
+    .then( function( coll ) {
+      event.steps = coll.toJSON();
+
+      return event;
+    });
   },
 
 }, {
